1b1b825a72f0cbd8295bd9dc764b79f2f1bc61df
Lionel Herrero Battista <lionherrero@gmail.com>
2022-06-20 16:11:01 -0300

    Completed Section 3

--- section3/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ section3/course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://titaniumfitnessllc.net/wp-content/uploads/3D-Cover-Art-Phase3-Png8-600PX-dark.png'),
+    new Recipe('A second test recipe', 'This is simply a test', 'https://titaniumfitnessllc.net/wp-content/uploads/3D-Cover-Art-Phase3-Png8-600PX-dark.png'),
+  ];
 
   constructor() { }
 
---
+++ section3/course-project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+    //constructor short form prop definition + assignment
+    constructor(public name: string, public amount: number) { }
+}
