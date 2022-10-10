4b3bdc980b2822eba259effd47fe9193a183672a
Lionel Herrero Battista <lionherrero@gmail.com>
2022-10-10 14:32:56 -0300

    finished section 12

---
+++ section12_course_project/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+  recipe: Recipe;
+  index: number;
+
+  constructor(private recipeService: RecipeService, private router:Router,  private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      this.index = +params['id'];     
+      this.recipe = this.recipeService.getRecipe(this.index);
+    })
+  }
+
+  onAddShoppingList(){
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+  }
+
+}
