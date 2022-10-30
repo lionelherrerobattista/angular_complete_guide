375cd56ff16ac3ce223cdbdf29d8e860f6775f7b
Lionel Herrero Battista <lionherrero@gmail.com>
2022-10-30 16:08:14 -0300

    Finished section 18 - http

--- section18_http_requests/http-01-start/src/app/posts.service.ts
+++ section18_http_requests/http-01-start/src/app/posts.service.ts
@@ -1,33 +1,86 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { catchError, map, tap } from "rxjs/operators";
 import { Post } from "./post.model";
 
-@Injectable({providedIn: 'root'})
-export class PostsService { 
-    constructor(private http: HttpClient) {}
+@Injectable({ providedIn: "root" })
+export class PostsService {
+  error = new Subject<string>();
 
-    createAndStorePost(title:string, content: string) {
-        const postData: Post = {title: title, content: content};
-        this.http.post<{name:string}>(
-            'https://ng-complete-guide-936fb-default-rtdb.firebaseio.com/posts.json',
-             postData).subscribe(responseData => {
-              
-              console.log(responseData);
-          });
-    }
+  constructor(private http: HttpClient) {}
 
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
+    this.http
+      .post<{ name: string }>(
+        "https://ng-complete-guide-936fb-default-rtdb.firebaseio.com/posts.json",
+        postData,
+        {
+          observe: "response",
+        }
+      )
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
+  }
 
-    fetchPosts() {
-        return this.http.get<{[key: string]:Post}>('https://ng-complete-guide-936fb-default-rtdb.firebaseio.com/posts.json')
-            .pipe(map(responseData => {
-                const postsArray: Post[] = [];
-                for(const key in responseData){
-                if(responseData.hasOwnProperty(key)){
-                    postsArray.push({...responseData[key], id: key});
-                }
-                }
-                return postsArray;
-            }));
-    }
+  fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append("print", "pretty");
+    searchParams = searchParams.append("custom", "key");
+
+    return this.http
+      .get<{ [key: string]: Post }>(
+        "https://ng-complete-guide-936fb-default-rtdb.firebaseio.com/posts.json",
+        {
+          headers: new HttpHeaders({ "Custom-Header": "Hello" }),
+          params: searchParams,
+          responseType: 'json'
+        }
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        }),
+        catchError((errorRes) => {
+          //Send to analytics server
+          return throwError(errorRes);
+        })
+      );
+  }
+
+  deletePosts() {
+    return this.http
+      .delete(
+        "https://ng-complete-guide-936fb-default-rtdb.firebaseio.com/posts.json",
+        {
+          observe: "events",
+          responseType: 'text'
+        }
+      )
+      .pipe(
+        tap((event) => {
+          console.log(event);
+          if(event.type === HttpEventType.Sent) {
+            // ...
+          }
+          if(event.type === HttpEventType.Response){
+            console.log(event.body);
+            
+          }
+        })
+      );
+  }
 }
