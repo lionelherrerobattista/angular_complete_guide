2cb44b27699ea540c84756f887fad30a59576f75
Lionel Herrero Battista <lionherrero@gmail.com>
2022-11-05 17:38:13 -0300

    Finished section 22 - modules

---
+++ section22_modules/src/app/recipes/recipes-resolver.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@angular/core";
+import {
+  ActivatedRouteSnapshot,
+  Resolve,
+  RouterStateSnapshot,
+} from "@angular/router";
+
+import { DataStorageService } from "../shared/data-storage.service";
+import { Recipe } from "./recipe.model";
+import { RecipeService } from "./recipe.service";
+
+@Injectable({ providedIn: "root" })
+export class RecipesResolverService implements Resolve<Recipe[]> {
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const recipe = this.recipeService.getRecipes();
+
+    if(recipe.length === 0) {
+        return this.dataStorageService.fetchRecipes();
+    } else {
+        return recipe;
+    }
+    
+  }
+}
